f0cee3702980a82fad13487099dcf7a20f90c23a
knavero <knavero@gmail.com>
2013-12-19 19:34:10 -0800

    did a clean

--- Debug/src/main/SDLAbstractionLayer.d
+++
@@ -1,6 +0,0 @@
-src/main/SDLAbstractionLayer.d: ../src/main/SDLAbstractionLayer.cpp \
- ../src/main/SDLAbstractionLayer.h ../src/main/Exception.h
-
-../src/main/SDLAbstractionLayer.h:
-
-../src/main/Exception.h:
